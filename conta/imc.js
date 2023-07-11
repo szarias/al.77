d371d3e0d61605d9c69af7b08cea926f357ebcc4
ariasz <129909951+szarias@users.noreply.github.com>
2023-07-10 21:51:57 -0300

    n func
    calc/ imc

---
+++ conta/imc.js
@@ -0,0 +1,10 @@
+let titulo = document.getElementById("title");
+
+titulo.innerHTML = "Veja o IMC";
+
+function imc(peso, altura){
+    let resultado = document.getElementById
+    ('Resultado')
+    resultado.innerText = (peso / (altura * altura))
+}
+
---
+++ conta/soma.js
@@ -0,0 +1,9 @@
+const resultado = document.getElementById("Resultado");
+
+resultado.addEventListener("click", function soma(){
+    const num1 = parseInt(document.getElementById("numero1").value);
+    const num2 = parseInt(document.getElementById("numero2").value);
+
+    let total = document.getElementById("total");
+    total.text
+})
